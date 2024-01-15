e92097e2ce6a8de934965aef1b83ca2ed7ffb808
adamem02 <adamem619@gmail.com>
2024-01-14 18:35:58 -0800

    js and gitignore

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,54 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    //add more questions based on the README structure
+];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+      err ? console.error(err) : console.log('README.md successfully generated!')
+    );
+  }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    //prompt for user info
+    inquirer
+    .prompt(questions)
+    .then((answer) => {
+        //Genrate the README content based on user input
+        const readMeContent = generateREADME(answers);
+
+        //write the README file
+        writeToFile('README.md' , readMeContent);
+    })
+    .catch((error) => console.error(error));
+
+}
+//Function to generate the README content
+function generateREADME(answer) {
+    const readmeContent = `
+    # ${answers.title}
+    
+    ## Description
+    ${answers.description}
+    
+    // ... other sections
+  `;
+
+  return readmeContent;  
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,47 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown'); // Add the correct path
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Enter the title of your project:',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Describe your project:',
+  },
+  // Add more questions based on the README structure
+];
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log('README.md successfully generated!')
+  );
 }
+
+// TODO: Create a function to initialize app
+function init() {
+  // Prompt the user for information
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      // Generate the README content based on user input
+      const readmeContent = generateMarkdown(answers);
+
+      // Write the README file
+      writeToFile('README.md', readmeContent);
+    })
+    .catch((error) => console.error(error));
+}
+
+// Function call to initialize app
+init();
+
 
 module.exports = generateMarkdown;
