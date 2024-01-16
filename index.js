92b9f0a1f672bd89f3f511813df52dbe70eb6164
adamem02 <adamem619@gmail.com>
2024-01-15 18:54:38 -0800

    created README uploaded final code

--- index.js
+++ index.js
@@ -2,76 +2,88 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const path = require("path");
-const generateMarkdown = require("./utils/generateMarkdown.js");
+const generateMarkdown = require("./utils/generateMarkdown");
 const { type } = require("os");
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        type: "input",
-        name: "title",
-        message: "What is the title of your project?",
+      type: "input",
+      name: "title",
+      message: "Please name your Project.",
     },
     {
-        type: "input",
-        name: "description",
-        message: "Provide a description of your project:"
+      type: "input",
+      name: "description",
+      message: "Please describe the purpose and functionality of this project.",
     },
     {
-        type: "list",
-        name:"link",
-        message: "Where can I find the source code for your project?",
+      type: "input",
+      name: "screenshot",
+      message: "Please provide the relative path to the image you want to use as the screenshot."
     },
     {
-        type:"checkbox",
-        name:"license",
-        message:"Select license for this project",
-        choices:["MIT", "APACHE2.0", "Boost1,0", "none"],
-    }
-    {
-        type:"input",
-        name:"require",
-        message:"Are there any dependencies to list?"
+      type: "input",
+      name: "link",
+      message: "Please provide a URL where a user can access your deployed application."
     },
     {
-        type:"input",
-        name:"features",
-        message:"List some features about this project",
-    }
+      type: "checkbox",
+      name: "license",
+      message: "Please select a license applicable to this project.",
+      choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD2", "BSD3", "none"],
+    },
     {
-        type: "input",
-        name: "creator",
-        message: "Write your GitHub username.",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "Provide a valid email address.",
-      },
-      {
-        type: "input",
-        name: "contributors",
-        message: "Please list any contributors. (Use GitHub usernames)",
-        default: "",
-      },
-      {
-        type: "input",
-        name: "test",
-        message: "Provide walkthrough of required tests if applicable.",
-      },
-];
-
-// TODO: Create a function to write README file
+      type: "input",
+      name: "require",
+      message: "List any project dependencies here.",
+    },
+    {
+      type: "input",
+      name: "features",
+      message: "List some features about this project here.",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message:
+        "State the languages or technologies associated with this project.",
+    },
+    {
+      type: "input",
+      name: "creator",
+      message: "Write your GitHub username.",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Provide a valid email address.",
+    },
+    {
+      type: "input",
+      name: "contributors",
+      message: "Please list any contributors. (Use GitHub usernames)",
+      default: "",
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "Provide walkthrough of required tests if applicable.",
+    },
+  ];
+  
+  
+// Writing README.md File
 function writeToFile(fileName, data) {
-    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+  return fs.writeFileSync(path.join(process.cwd(), "utils" , fileName), data);
 }
 
-// TODO: Create a function to initialize app
+// Initializing app
 function init() {
-    inquirer.prompt(questions).then((responses) => {
-      console.log("Creating Professional README.md File...");
-      writeToFile("./dist/README.md", generateMarkdown({ ...responses }));
-    });
-  }
-  // Function call to initialize app
-  init();
+  inquirer.prompt(questions).then((responses) => {
+    console.log("Creating Professional README.md File...");
+
+    writeToFile("README.md" , generateMarkdown({ ...responses }));
+  });
+}
+init();
---
+++ utils/README.md
@@ -0,0 +1,29 @@
+# README Generator
+  ![Github license](https://img.shields.io/badge/license--blue.svg)
+  ## Description
+  As a developer
+I want a README generator
+so that I can quickly create a professional README for a new project
+  ## Deployed Application URL
+  https://github.com/adamem02/Noodles
+  ## Demo
+  <video src="../README%20Generator.mp4" controls title="Title"></video>)
+  ## Table of Contents
+  * [Features](#features)
+  * [Languages & Dependencies](#languagesanddependencies)
+  * [How to Use This Application](#HowtoUseThisApplication)
+  * [Contributors](#contributors)
+  * [Testing](#testing)
+  * [Questions](#questions)
+  ## Features
+  n/a
+  ## Languages & Dependencies
+  n/a
+  ## How to Use This Application:
+  JavaScript, node.js
+  ## Contributors
+  none
+  ## Testing
+  n/a
+  ## Questions
+  Please send your questions [here](mailto:adamem619@gmail.com?subject=[GitHub]%20Dev%20Connect) or visit [github/adamem02](https://github.com/adamem02).
